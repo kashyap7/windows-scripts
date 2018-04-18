5021be048466dab7e6ae89b75bffa6d04588844c
kashyap7 <kashyap.skash@gmail.com>
2018-04-18 16:18:34 +0000

    correcting the process-cron script

--- restart-process-cron.bat
+++ restart-process-cron.bat
@@ -1,5 +1,6 @@
-rem usage: test.bat <process you don't want to end>
+
 :loop
-tasklist /FI "IMAGENAME eq %1" 2>NUL | find /I /N "%1">NUL
+tasklist | find /I /N "%1">NUL
 if NOT "%ERRORLEVEL%"=="0" %1
+TIMEOUT /T 5
 goto loop
